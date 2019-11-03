82f571ee1069711b1cfc26416b0ff4c3cefefde5
e142766 <praveen.shastrula@wnco.com>
2019-11-03 10:26:02 -0600

    Added docker-compose

--- src/main/java/com/aero/flights/flightinfo/message/ConnectionFactory.java
+++ src/main/java/com/aero/flights/flightinfo/message/ConnectionFactory.java
@@ -1,14 +1,19 @@
 package com.aero.flights.flightinfo.message;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ConnectionFactory {
 
+    @Value("${ACTIVE_MQ_BROKER_URL}")
+    private String brokerURL;
+
     public javax.jms.ConnectionFactory connectionFactory() {
+        brokerURL = "vm://localhost";
         javax.jms.ConnectionFactory connectionFactory =
-                new ActiveMQConnectionFactory("vm://localhost");
+                new ActiveMQConnectionFactory(brokerURL);
         return connectionFactory;
     }
 
