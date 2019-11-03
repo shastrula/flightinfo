7cd0bb28090ae4987efe3736c7d6e7f50f4f4ced
e142766 <praveen.shastrula@wnco.com>
2019-11-03 11:04:30 -0600

    Updated for activemq

--- src/main/java/com/aero/flights/flightinfo/message/ConnectionFactory.java
+++ src/main/java/com/aero/flights/flightinfo/message/ConnectionFactory.java
@@ -7,11 +7,9 @@ import org.springframework.stereotype.Component;
 @Component
 public class ConnectionFactory {
 
-    @Value("${ACTIVE_MQ_BROKER_URL}")
     private String brokerURL;
 
     public javax.jms.ConnectionFactory connectionFactory() {
-        brokerURL = "vm://localhost";
         javax.jms.ConnectionFactory connectionFactory =
                 new ActiveMQConnectionFactory(brokerURL);
         return connectionFactory;
