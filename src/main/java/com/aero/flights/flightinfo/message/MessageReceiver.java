6d440327fe2d1e7ba2edeb6bb35659793620bb27
e142766 <praveen.shastrula@wnco.com>
2019-11-03 08:04:13 -0600

    Simpified Receiver

--- src/main/java/com/aero/flights/flightinfo/message/MessageReceiver.java
+++ src/main/java/com/aero/flights/flightinfo/message/MessageReceiver.java
@@ -2,26 +2,12 @@ package com.aero.flights.flightinfo.message;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
-import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Component;
-
-import javax.annotation.PostConstruct;
-import javax.jms.ConnectionFactory;
 
 @Component
 public class MessageReceiver {
     private static Logger logger = LoggerFactory.getLogger(MessageReceiver.class);
-
-    @Autowired
-    private ConnectionFactory connectionFactory;
-    private JmsTemplate jmsTemplate;
-
-    @PostConstruct
-    public void init() {
-        this.jmsTemplate = new JmsTemplate(connectionFactory);
-    }
 
     @JmsListener(destination = "flight-queue")
     public void receiveMessage(String receivedText) {
